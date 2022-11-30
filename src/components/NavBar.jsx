94d478e2135410f6299952b9cbcc667aa86721db
cherif <cheriffzouaoui@gmail.com>
2022-11-30 21:20:01 +0100

    some navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,20 @@ import {logo, flag} from "../assets"
 import { dropdown } from '../data'
 import { AiOutlineSearch } from "react-icons/ai";
 import {CgShoppingCart} from "react-icons/cg"
+import {HiOutlineLocationMarker} from "react-icons/hi"
 
 const NavBar = () => {
   return (
-    <div className=''>
-        <div className='flex justify-around nav-bg w-full text-white p-[7px]'>
-            <img src={logo} alt="Amazon" className='w-[100px] h-[30px] mr-5 place-self-center'/>
-            <div className='flex-none mr-5'>
-                <p>Deliver to</p>
-                <p>Algeria</p>
+    <div>
+        <div className='flex justify-around nav-bg w-full text-white p-[7px] h-[60px]'>
+            <div className='w-[120px] flex-none place-self-center hover:border-[1px] border-white cursor-pointer'>
+              <img src={logo} alt="Amazon" className='h-[30px] m-1'/>
+            </div>
+            <div className='flex flex-none mr-5 hover:border-[1px] border-white cursor-pointer items-center p-2'>
+              {/* <div className='flex items-end'> */}
+                <HiOutlineLocationMarker  className='mr-[2px] mt-auto'/>
+              {/* </div> */}
+              <p className='text-[12px] text-[#ccc] leading-[15px]'>Deliver to <br /><span className='text-[14px] text-white font-bold'>Algeria</span></p>
             </div>
             <div className=' flex grow mr-5 sm:w-full p-1'>
               <select name="sections" id="sections" className='text-black w-[50px] rounded-l bg-slate-200 '>
