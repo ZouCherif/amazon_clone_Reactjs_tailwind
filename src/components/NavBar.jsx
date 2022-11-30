e9d24f39327fa93b9512499db48d109db530857a
cherif <cheriffzouaoui@gmail.com>
2022-11-30 15:57:01 +0100

    navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,33 +6,33 @@ import {CgShoppingCart} from "react-icons/cg"
 
 const NavBar = () => {
   return (
-    <div className='flex'>
-        <div className='flex nav-bg w-full text-white p-[7px] content-around'>
-            <img src={logo} alt="Amazon" className='w-[90px] h-[40px]'/>
-            <div className=''>
+    <div className=''>
+        <div className='flex justify-around nav-bg w-full text-white p-[7px]'>
+            <img src={logo} alt="Amazon" className='w-[100px] h-[30px] mr-5 place-self-center'/>
+            <div className='flex-none mr-5'>
                 <p>Deliver to</p>
                 <p>Algeria</p>
             </div>
-            <div className='h-8'>
-              <select name="sections" id="sections" className='text-black w-[50px] h-full rounded-l bg-slate-200 '>
+            <div className=' flex grow mr-5 sm:w-full p-1'>
+              <select name="sections" id="sections" className='text-black w-[50px] rounded-l bg-slate-200 '>
                 {dropdown.map((opt) => <option value={opt}>{opt}</option> )}
               </select>
-              <input type="text" className='h-full'/>
-              <button className='bg-orange-300 w-[40px] h-full rounded-r '><AiOutlineSearch className='fill-black'/></button>
+              <input type="text" className='grow'/>
+              <button className='bg-orange-300 w-[40px] rounded-r'><AiOutlineSearch className='fill-black'/></button>
             </div>
-            <div className='flex items-center'>
+            <div className='flex items-center flex-none mr-5'>
               <img src={flag} alt="usa" className='h-[17px] w-[25px] pr-[3px]' />
               <p className='font-bold'>EN</p>
             </div>
-            <div>
+            <div className='flex-none mr-5'>
               <p>Hello, sign in</p>
               <p className='font-bold'>Account & Lists</p>
             </div>
-            <div>
+            <div className='flex-none mr-5'>
               <p>Returns</p>
               <p className='font-bold'>& Orders</p>
             </div>
-            <div className='flex'>
+            <div className='flex flex-none mr-5'>
               <CgShoppingCart className=""/>
               <p>Cart</p>
             </div>
