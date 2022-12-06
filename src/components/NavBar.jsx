12982e5ffcaaa7ed4afaaeefcfae54956828a2bc
cherif <cheriffzouaoui@gmail.com>
2022-12-06 09:57:27 +0100

    under_navbar_Done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,11 +22,11 @@ const NavBar = () => {
               <p className='text-[12px] text-[#ccc]'>Deliver to <br /><span className='text-[14px] text-white font-bold'>Algeria</span></p>
             </div>
             <div className=' flex grow mr-4 sm:w-full p-1'>
-              <select name="sections" id="sections" className='text-black w-[50px] rounded-l bg-slate-200 '>
+              <select name="sections" id="sections" className='text-black w-[50px] rounded-l bg-slate-200 text-[14px] text-center'>
                 {dropdown.map((opt) => <option value={opt}>{opt}</option> )}
               </select>
               <input type="text" className='grow text-black pl-2 '/>
-              <button className='bg-orange-300 w-[40px] rounded-r'><AiOutlineSearch className='fill-black'/></button>
+              <button className='bg-orange-300 w-[40px] rounded-r pl-1'><AiOutlineSearch size={30} className='fill-black'/></button>
             </div>
             <div className='flex items-center flex-none mr-4 border-[1px] border-transparent hover:border-white cursor-pointer p-2 relative' 
             onMouseEnter={() => setLanguage(true)}
@@ -52,7 +52,7 @@ const NavBar = () => {
               <p className='text-[14px] font-bold'>Cart</p>
             </div>
         </div>
-        <div className='flex p-[6px] pl-4 bg-[#232f3e] text-white h-fit text-[14px] font-bold'>
+        <div className='flex p-[3px] pl-4 bg-[#232f3e] text-white h-fit text-[14px] font-bold'>
           <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'>
             <p className='flex items-center'><BsList className='mr-[2px]' size={23} strokeWidth={0.4}/>All</p>
           </div>
