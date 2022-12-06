5091efe5b623e4993c3542bc77ee0796e2f59ee7
cherif <cheriffzouaoui@gmail.com>
2022-12-06 08:28:31 +0100

    under_navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import {logo, flag} from "../assets"
 import { dropdown } from '../data'
 import { AiOutlineSearch } from "react-icons/ai";
 import {CgShoppingCart} from "react-icons/cg"
+import { BsList } from "react-icons/bs";
 import {HiOutlineLocationMarker} from "react-icons/hi"
 import LanguageDropdownMenu from './LanguageDropdownMenu'
 import AccountLists_Dropdown_menu from './AccountLists_Dropdown_menu';
@@ -12,11 +13,11 @@ const NavBar = () => {
   const [accountLists, setaccountLists] = React.useState(false)
   return (
     <div>
-        <div className='flex justify-around nav-bg w-full text-white p-[7px] h-[60px] leading-[15px]'>
-            <div className='w-[120px] flex-none place-self-center hover:border-[1px] cursor-pointer'>
+        <div className='flex justify-around nav-bg w-full text-white p-[6px] h-[60px] leading-[15px]'>
+            <div className='w-[120px] flex-none place-self-center border-[1px] border-transparent hover:border-white cursor-pointer'>
               <img src={logo} alt="Amazon" className='h-[30px] m-1'/>
             </div>
-            <div className='flex flex-none mr-5 hover:border-[1px] cursor-pointer items-center p-2'>
+            <div className='flex flex-none mr-5 border-[1px] border-transparent hover:border-white cursor-pointer items-center p-2'>
               <HiOutlineLocationMarker  className='mr-[2px] mt-auto'/>
               <p className='text-[12px] text-[#ccc]'>Deliver to <br /><span className='text-[14px] text-white font-bold'>Algeria</span></p>
             </div>
@@ -27,14 +28,14 @@ const NavBar = () => {
               <input type="text" className='grow text-black pl-2 '/>
               <button className='bg-orange-300 w-[40px] rounded-r'><AiOutlineSearch className='fill-black'/></button>
             </div>
-            <div className='flex items-center flex-none mr-4 hover:border-[1px] cursor-pointer p-2 relative' 
+            <div className='flex items-center flex-none mr-4 border-[1px] border-transparent hover:border-white cursor-pointer p-2 relative' 
             onMouseEnter={() => setLanguage(true)}
             onMouseLeave={() => setLanguage(false)}>
               <img src={flag} alt="usa" className='h-[17px] w-[25px] pr-[3px]'/>
               <p className='font-bold text-[14px]'>EN</p>
               {language && <LanguageDropdownMenu />}
             </div>
-            <div className='flex-none mr-4 hover:border-[1px] cursor-pointer pt-2 px-1 relative'
+            <div className='flex-none mr-4 border-[1px] border-transparent hover:border-white cursor-pointer pt-2 px-1 relative'
             onMouseEnter={() => setaccountLists(true)}
             onMouseLeave={() => setaccountLists(false)}
             >
@@ -42,14 +43,25 @@ const NavBar = () => {
               <p className='font-bold text-[14px]'>Account & Lists</p>
               {accountLists && <AccountLists_Dropdown_menu />}
             </div>
-            <div className='flex-none mr-4 hover:border-[1px] cursor-pointer pt-2 px-1'>
+            <div className='flex-none mr-4 border-[1px] border-transparent hover:border-white cursor-pointer pt-2 px-1'>
               <p className='text-[12px]'>Returns</p>
               <p className='font-bold text-[14px]'>& Orders</p>
             </div>
-            <div className='flex flex-none mr-3 items-center hover:border-[1px] cursor-pointer p-2'>
+            <div className='flex flex-none mr-3 items-center border-[1px] border-transparent hover:border-white cursor-pointer p-2'>
               <CgShoppingCart size={35}/>
               <p className='text-[14px] font-bold'>Cart</p>
             </div>
+        </div>
+        <div className='flex p-[6px] pl-4 bg-[#232f3e] text-white h-fit text-[14px] font-bold'>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'>
+            <p className='flex items-center'><BsList className='mr-[2px]' size={23} strokeWidth={0.4}/>All</p>
+          </div>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'><p>Today's Deals</p></div>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'><p>Customer Service</p></div>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'><p>Registry</p></div>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'><p>Gift Cards</p></div>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1'><p>Sell</p></div>
+          <div className='cursor-pointer mr-3 border-[1px] border-transparent hover:border-white p-1 ml-auto'><p>Shop great deals now</p></div>
         </div>
     </div>  
   )
