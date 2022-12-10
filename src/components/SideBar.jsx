b5563c8169afacd878a1219328bca6bf7bce0cc8
cherif <cheriffzouaoui@gmail.com>
2022-12-10 17:35:33 +0100

    sidebar_scroll

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,11 +11,11 @@ export const SideBar = (props) => {
 
   return (
     <div className={`z-20 absolute h-screen w-[350px] bg-white ${props.isOpen? 'translate-x-0' :'-translate-x-full'} ease-in-out duration-500 top-0 left-0`}>
-      <div className='cursor-pointer flex bg-[#232f3e] text-white p-2'>
+      <div className='cursor-pointer flex bg-[#232f3e] text-white h-[6.5%] p-2'>
         <div className="w-[30px] h-[30px]"></div>
         <p className='text-[19px] font-[700]'>Hello, Sign in</p>
       </div>
-      <div className='overflow-y-scroll'>
+      <div className='h-[93.5%] overflow-scroll'>
         {sideBar.map((item)=>(
           <div>
             <h2 className='font-bold'>{item.title}</h2>
